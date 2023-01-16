281ebeb0fa8e224101035ff10853e7375f7a1a03
yunpeng.li <yunpeng.li@vaneplus.com>
2023-01-16 17:02:19 +0800

    #202301161703 add stpslice.Compare

---
+++ slice/slice.go
@@ -0,0 +1,14 @@
+package stpslice
+
+func Compare[T comparable](s1, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	l := len(s1)
+	for index := 0; index != l; index++ {
+		if s1[index] != s2[index] {
+			return false
+		}
+	}
+	return true
+}
