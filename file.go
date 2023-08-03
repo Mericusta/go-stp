826febb124d1e9b95e1b3d811d97488508973345
yunpeng.li <yunpeng.li@vaneplus.com>
2023-08-03 15:53:58 +0800

    #202308031553 update file

--- file.go
+++ file.go
@@ -13,7 +13,7 @@ import (
 )
 
 // ReadFileLineOneByOne 逐行读取文件内容，执行函数返回 true 则继续读取，返回 false 则结束读取
-func ReadFileLineOneByOne(filename string, f func(string) bool) error {
+func ReadFileLineOneByOne(filename string, f func(string, int) bool) error {
 	file, openError := os.Open(filename)
 	if openError != nil {
 		return openError
@@ -24,11 +24,11 @@ func ReadFileLineOneByOne(filename string, f func(string) bool) error {
 }
 
 // ReadContentLineOneByOne 逐行读取指定内容，执行函数返回 true 则继续读取，返回 false 则结束读取
-func ReadContentLineOneByOne(reader io.Reader, f func(string) bool) error {
-	scanner := bufio.NewScanner(reader)
+func ReadContentLineOneByOne(reader io.Reader, f func(string, int) bool) error {
+	index, scanner := 0, bufio.NewScanner(reader)
 
 	for scanner.Scan() {
-		if !f(scanner.Text()) {
+		if !f(scanner.Text(), index) {
 			break
 		}
 	}
